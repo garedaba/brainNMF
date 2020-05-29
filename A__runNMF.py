938e6fa84c29b290a4ec49815c40e2b60cb9f83a
Gareth Ball <gareth.ball@mcri.edu.au>
2020-05-29 16:34:52 +1000

    Update runNMF
    change option parsing

--- A__runNMF.py
+++ A__runNMF.py
@@ -37,7 +37,12 @@ def main():
 
     # other params
     preprocessing_params = cfg['preprocessing']
+    wins = 'Wins' if preprocessing_params['winsorise'] else 'NoWins'
+    cbt = 'Combat' if preprocessing_params['combat'] else 'noCombat'
+    unorm = 'Norm' if preprocessing_params['unit_norm'] else 'noNorm'
+
     nmf_params = cfg['nmf']
+    subj = nmf_params['decomp_data']
 
     # load meta data
     meta_data = pd.read_csv(datapath + 'subject-data.csv')
@@ -72,16 +77,12 @@ def main():
             print('')
             surface_data = np.log1p(surface_data)
 
-        wins = 'noWins'
         if preprocessing_params['winsorise']:
-            wins = 'Wins'
             print('flattening top and bottom 1% of values')
             print('')
             surface_data = winsorize(surface_data, limits=(.01), axis=1).data
 
-        cbt = 'noCombt'
         if preprocessing_params['combat']:
-            cbt = 'Combt'
             print('performing comBat harmonisation')
             print('')
             # combat
@@ -89,62 +90,66 @@ def main():
             # remove vertices <=0
             surface_data[surface_data<0]=0
 
-        unrm = 'noNorm'
         if preprocessing_params['unit_norm']:
-            unrm = 'Norm'
             print('normalising individual data to unit norm')
             print('')
             surface_data  = normalise_features(surface_data)
 
-        subj = 'ALL'
-        if nmf_params['decomp_data'] == 'hc':
-            subj = 'HC'
+        if subj == 'hc':
             print('performing NMF using healthy control data ONLY')
             surface_data = surface_data[meta_data.diagnosis=='CONTROL',:]
 
-        print('')
-        print('*********************************************************')
-        print('RUNNING NMF')
-        print('*********************************************************')
-        nmf_results = ARDPNMF(surface_data.T, nmf_params['init_comps'],
-                                              init=nmf_params['init'],
-                                              maxIter=nmf_params['n_iter'],
-                                              tol=1e-6)
+        if os.path.exists('{:}{:}-nmf-loadings-{:}-{:}-{:}-{:}.csv'.format(outpath, metric, wins, cbt, unorm, subj)):
+            print('')
+            print('*********************************************************')
+            print('NMF for {:} already performed. See GeneratedData/'.format(metric))
+            print('*********************************************************')
 
-        print('')
-        # components
-        basis_components = nmf_results['W']
-        basis_components = basis_components[:,np.argsort(-nmf_results['norms'])]
-        basis_components = basis_components[:,nmf_results['norms']>0]
+        else:
+            print('')
+            print('*********************************************************')
+            print('RUNNING NMF')
+            print('*********************************************************')
+            nmf_results = ARDPNMF(surface_data.T, nmf_params['init_comps'],
+                                                  init=nmf_params['init'],
+                                                  maxIter=nmf_params['n_iter'],
+                                                  tol=1e-6)
 
-        # subject weights
-        subject_loadings = basis_components.T.dot(surface_data.T).T
+            print('')
+            # components
+            basis_components = nmf_results['W']
+            basis_components = basis_components[:,np.argsort(-nmf_results['norms'])]
+            basis_components = basis_components[:,nmf_results['norms']>0]
 
-        # save out
-        print('---------------------------------------------------------')
-        print('saving NMF loadings to: {:}{:}-nmf-loadings-{:}-{:}-{:}-{:}.csv'.format(outpath, metric, wins, cbt, unrm, subj))
-        nmf_loadings = pd.concat((meta_data, pd.DataFrame(subject_loadings)), axis=1)
-        nmf_loadings.to_csv('{:}{:}-nmf-loadings-{:}-{:}-{:}-{:}.csv'.format(outpath, metric, wins, cbt, unrm, subj), index=False)
-        print('---------------------------------------------------------')
-        print('saving NMF weights to: {:}{:}-nmf-components-{:}-{:}-{:}-{:}.npy'.format(outpath, metric, wins, cbt, unrm, subj))
-        print('zero vector to: {:}{:}-nmf-zero-data.npy'.format(outpath, metric))
-        np.save('{:}{:}-nmf-components-{:}-{:}-{:}-{:}.npy'.format(outpath, metric, wins, cbt, unrm, subj), basis_components)
-        np.save('{:}{:}-nmf-zero-data.npy'.format(outpath, metric), zero_data)
-        print('---------------------------------------------------------')
-        print('')
+            # subject weights
+            subject_loadings = basis_components.T.dot(surface_data.T).T
 
-        # save surface components
-        print('saving NMF component maps')
-        os.makedirs(outpath + 'surface_components', exist_ok=True)
-        for comp in np.arange(np.shape(basis_components)[1]):
-            comp_data = np.zeros(np.shape(zero_data))
-            comp_data[~zero_data] = basis_components[:,comp]
-            save_surface_out(comp_data[:10242], outpath + 'surface_components', hemi='lh', template='fsaverage5')
-            os.rename(outpath + 'surface_components/lh.out_data.mgh', outpath + 'surface_components/lh.comp{:02d}-{:}-{:}-{:}-{:}-{:}.mgh'.format(comp+1, metric, wins, cbt, unrm, subj))
-            save_surface_out(comp_data[10242:], outpath + 'surface_components', hemi='rh', template='fsaverage5')
-            os.rename(outpath + 'surface_components/rh.out_data.mgh', outpath + 'surface_components/rh.comp{:02d}-{:}-{:}-{:}-{:}-{:}.mgh'.format(comp+1, metric, wins, cbt, unrm, subj))
-        print('see ' + outpath + 'surface_components/?h.comp??.*.mgh')
-        print('combine into 4D file using Freesurfer')
+            # save out
+            print('---------------------------------------------------------')
+            print('saving NMF loadings to: {:}{:}-nmf-loadings-{:}-{:}-{:}-{:}.csv'.format(outpath, metric, wins, cbt, unorm, subj))
+            column_labels = [('{:}{:02d}'.format(metric, i+1)) for i in np.arange(np.shape(subject_loadings)[1])]
+            nmf_loadings = pd.concat((meta_data, pd.DataFrame(subject_loadings, columns=column_labels)), axis=1)
+            nmf_loadings.to_csv('{:}{:}-nmf-loadings-{:}-{:}-{:}-{:}.csv'.format(outpath, metric, wins, cbt, unorm, subj), index=False)
+            print('---------------------------------------------------------')
+            print('saving NMF weights to: {:}{:}-nmf-components-{:}-{:}-{:}-{:}.npy'.format(outpath, metric, wins, cbt, unorm, subj))
+            print('zero vector to: {:}{:}-nmf-zero-data.npy'.format(outpath, metric))
+            np.save('{:}{:}-nmf-components-{:}-{:}-{:}-{:}.npy'.format(outpath, metric, wins, cbt, unorm, subj), basis_components)
+            np.save('{:}{:}-nmf-zero-data.npy'.format(outpath, metric), zero_data)
+            print('---------------------------------------------------------')
+            print('')
+
+            # save surface components
+            print('saving NMF component maps')
+            os.makedirs(outpath + 'surface_components', exist_ok=True)
+            for comp in np.arange(np.shape(basis_components)[1]):
+                comp_data = np.zeros(np.shape(zero_data))
+                comp_data[~zero_data] = basis_components[:,comp]
+                save_surface_out(comp_data[:10242], outpath + 'surface_components', hemi='lh', template='fsaverage5')
+                os.rename(outpath + 'surface_components/lh.out_data.mgh', outpath + 'surface_components/lh.comp{:02d}-{:}-{:}-{:}-{:}-{:}.mgh'.format(comp+1, metric, wins, cbt, unorm, subj))
+                save_surface_out(comp_data[10242:], outpath + 'surface_components', hemi='rh', template='fsaverage5')
+                os.rename(outpath + 'surface_components/rh.out_data.mgh', outpath + 'surface_components/rh.comp{:02d}-{:}-{:}-{:}-{:}-{:}.mgh'.format(comp+1, metric, wins, cbt, unorm, subj))
+            print('see ' + outpath + 'surface_components/?h.comp??.*.mgh')
+            print('combine into 4D file using Freesurfer')
 
 if __name__ == '__main__':
     main()
